5f6af48cccf6438e0774e0f54ab1071986c746f3
AmirhBeigi <dev.amirhbeigi@gmail.com>
2021-07-14 19:15:20 +0430

    chore

--- components/layout/index.js
+++ components/layout/index.js
@@ -9,11 +9,27 @@ const Layout = ({ children }) => {
         <meta name="description" content="Amirhossein Beigi | Front-end Developer" />
         <meta name="og:title" content="Amirhossein Beigi | Front-end Developer" />
         <meta name="twitter:card" content="summary_large_image" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <title>AmirhBeigi</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5SHJ9ZQ');`,
+          }}
+        ></script>
       </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SHJ9ZQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      />
+
       {children}
     </div>
   );
