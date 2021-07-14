ef4e73dd1d56d572648ddfa6c20b9ef92f810150
AmirhBeigi <dev.amirhbeigi@gmail.com>
2021-07-14 19:55:50 +0430

    chore

--- components/layout/index.js
+++ components/layout/index.js
@@ -15,20 +15,15 @@ const Layout = ({ children }) => {
         <title>AmirhBeigi</title>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5SHJ9ZQ');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5KBPJRYWZ9');`,
           }}
         ></script>
+        <script asyn src="https://www.googletagmanager.com/gtag/js?id=G-5KBPJRYWZ9"></script>
       </Head>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SHJ9ZQ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}
-      />
 
       {children}
     </div>
